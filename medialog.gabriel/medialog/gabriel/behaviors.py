84a376e820931096f7ed0b9fd75b0c6c4069deee
Espen Moe-Nilssen <espenmoe-nilssen@Espen.lan>
2016-08-30 16:24:22 +0200

    =?

--- medialog.gabriel/medialog/gabriel/behaviors.py
+++ medialog.gabriel/medialog/gabriel/behaviors.py
@@ -28,7 +28,6 @@ class IDate(schema.Date):
             title=_(u"Dato"),
             min=datetime.date(2015, 5, 12),
             max=datetime.date.today(),
-            defaultFactory=datetime.date.today() - datetime.timedelta(1),
     )
     
 class IGabrielBehavior(form.Schema):
