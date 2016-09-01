05009bb4d4e3155755ff51f81239b76419dfbb82
Espen Moe-Nilssen <espenmoe-nilssen@Espen.lan>
2016-09-01 15:03:18 +0200

    hack until we can fix database server

--- medialog.gabriel/medialog/gabriel/browser/viewlets.py
+++ medialog.gabriel/medialog/gabriel/browser/viewlets.py
@@ -51,101 +51,105 @@ class GraphView(ViewletBase):
         yesterday = datetime.date.today() - datetime.timedelta(1)
         
         if context.dato != yesterday:  
-            dtype = context.dtype
-            trace = []
+            try: 
+                dtype = context.dtype
+                trace = []
        
-            date = yesterday.strftime("%Y%m%d")
-            day_url = 'http://146.185.167.10/resampledday/%s/' %dtype
-            #on its own line, in case of looping
-            json_url = day_url + date + '.json'
-            f = urllib.urlopen(json_url)   
-            jsonfile=f.read()
-            daydata=json.loads(jsonfile)
-            df = pd.DataFrame(daydata)
-            xaxis = df['ts'].replace(to_replace=':00:00 GMT', value='', regex=True)
-            df.head()
-            this_dive = pd.DataFrame(df['divedata'].values.tolist())
+                date = yesterday.strftime("%Y%m%d")
+                day_url = 'http://146.185.167.10/resampledday/%s/' %dtype
+                #on its own line, in case of looping
+                json_url = day_url + date + '.json'
+                f = urllib.urlopen(json_url)   
+                jsonfile=f.read()
+                daydata=json.loads(jsonfile)
+                df = pd.DataFrame(daydata)
+                xaxis = df['ts'].replace(to_replace=':00:00 GMT', value='', regex=True)
+                df.head()
+                this_dive = pd.DataFrame(df['divedata'].values.tolist())
     
-            #seven dives a day, usually
-            for i in range(1,this_dive.shape[1]):
-                this_preassure = pd.DataFrame(this_dive[i-1].values.tolist())
-                name=str(this_preassure['pressure(dBAR)'][0])
-                graphname = name + ' dBar '
+                #seven dives a day, usually
+                for i in range(1,this_dive.shape[1]):
+                    this_preassure = pd.DataFrame(this_dive[i-1].values.tolist())
+                    name=str(this_preassure['pressure(dBAR)'][0])
+                    graphname = name + ' dBar '
         
-                # Create a trace
-                trace.append(go.Scatter(
-                            x = xaxis,
-                            y = this_preassure[dtype],
-                            name = graphname,
-                ))
+                    # Create a trace
+                    trace.append(go.Scatter(
+                                x = xaxis,
+                                y = this_preassure[dtype],
+                                name = graphname,
+                    ))
             
-            layout = go.Layout(
-                height=1000,
-                width=1200,
-                autosize=True,
-                xaxis=dict(
-                title='Tidspunkt',
-                    titlefont=dict(
-                        family='Courier New, monospace',
-                        size=18,
-                        color='#7f7f7f'
-                    )
-                ),
-                yaxis=dict(
-                    title='Verdi',
-                    titlefont=dict(
-                        family='Courier New, monospace',
-                        size=18,
-                        color='#7f7f7f'
-                    )
-                )
-            )
-    
-            fig = go.Figure(data=trace)
-            plotly_html = plotly.offline.plot(fig, show_link=False, include_plotlyjs = False, output_type='div')
-
-            #and now the 3D graph
-            z = []
-        
-            #construct the 3d z
-            for i in range(0,len(df)):
-                this_z = pd.DataFrame(df['divedata'][i]).sort_values('pressure(dBAR)', ascending=True)
-                z.append(this_z[dtype])
-                #.values.tolist())
-
-            data = [
-                go.Surface(
-                z=  z,
-                x= pd.DataFrame(df['divedata'][0])['pressure(dBAR)'].sort_values(),
-                y = df['ts']
-                )
-            ]
-
-            layout = go.Layout(
-                autosize=True,
-                scene=dict(
+                layout = go.Layout(
+                    height=1000,
+                    width=1200,
+                    autosize=True,
                     xaxis=dict(
-                        title="Dybde"
+                    title='Tidspunkt',
+                        titlefont=dict(
+                            family='Courier New, monospace',
+                            size=18,
+                            color='#7f7f7f'
+                        )
                     ),
                     yaxis=dict(
-                        title="Tid"
+                        title='Verdi',
+                        titlefont=dict(
+                            family='Courier New, monospace',
+                            size=18,
+                            color='#7f7f7f'
+                        )
                     )
-                ),
-                width=900,
-                height=1000,
-                margin=dict(
-                    l=5,
-                    r=5,
-                    b=5,
-                    t=9
                 )
-            )
+    
+                fig = go.Figure(data=trace)
+                plotly_html = plotly.offline.plot(fig, show_link=False, include_plotlyjs = False, output_type='div')
 
-            fig = go.Figure(data=data, layout=layout)
-            plotly_html += plotly.offline.plot(fig, show_link=False, include_plotlyjs = False, output_type='div')
-            context.plotly_html = plotly_html
-            context.dato = yesterday
+                #and now the 3D graph
+                z = []
         
+                #construct the 3d z
+                for i in range(0,len(df)):
+                    this_z = pd.DataFrame(df['divedata'][i]).sort_values('pressure(dBAR)', ascending=True)
+                    z.append(this_z[dtype])
+                    #.values.tolist())
+
+                data = [
+                    go.Surface(
+                    z=  z,
+                    x= pd.DataFrame(df['divedata'][0])['pressure(dBAR)'].sort_values(),
+                    y = df['ts']
+                    )
+                ]
+
+                layout = go.Layout(
+                    autosize=True,
+                    scene=dict(
+                        xaxis=dict(
+                            title="Dybde"
+                        ),
+                        yaxis=dict(
+                            title="Tid"
+                        )
+                    ),
+                    width=900,
+                    height=1000,
+                    margin=dict(
+                        l=5,
+                        r=5,
+                        b=5,
+                        t=9
+                    )
+                )
+
+                fig = go.Figure(data=data, layout=layout)
+                plotly_html += plotly.offline.plot(fig, show_link=False, include_plotlyjs = False, output_type='div')
+                context.plotly_html = plotly_html
+                context.dato = yesterday
+            
+            except:
+                "Gabriel data ble ikke funnet"
+                
         return context.plotly_html
         
         
